495f8212ff52a2849964e3a887fa8fff4f5166bf
Dione Wilson <dionew1@gmail.com>
2017-09-03 11:07:48 -0600

    Add validation methods

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,40 @@
 class Board
 
+attr_reader :grid, :rows, :columns, :ships
+
+  def initialize
+    @grid = {"A1" => nil, "A2" => nil, "A3" => nil, "A4" => nil,
+            "B1" => nil, "B2" => nil, "B3" => nil, "B4" => nil,
+            "C1" => nil, "C2" => nil, "C3" => nil, "C4" => nil,
+            "D1" => nil, "D2" => nil, "D3" => nil, "D4" => nil}
+    @rows = ["A", "B", "C", "D"]
+    @columns = [1, 2, 3, 4]
+    @ships = []
+  end
+
+  def upcase_coordinates(coordinates)
+    coordinates.upcase
+  end
+
+  def split_coordinates(coordinates)
+    coordinates.split
+  end
+
+  def get_letters(split_coordinates)
+    split_coordinates.map do |coordinate|
+      coordinate[0]
+    end
+  end
+
+  def get_numbers(split_coordinates)
+    split_coordinates.map do |coordinate|
+      coordinate[1]
+    end
+  end
+
+  def detect_direction
+
+
+  end
+
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -11,4 +11,53 @@ class BoardTest < Minitest::Test
     assert_instance_of Board, board
   end
 
+  def test_upcase_coordinates
+    board = Board.new
+    actual = board.upcase_coordinates("a1 a2")
+    expected = "A1 A2"
+
+    assert_equal expected, actual
+  end
+
+  def test_split_player_coordinates_into_characters
+    board = Board.new
+    actual = board.split_coordinates("A1 A2")
+    expected = ["A1", "A2"]
+
+    assert_equal expected, actual
+  end
+
+  def upcase_coordinates
+    board = Board.new
+    actual = board.split_coordinates(board.upcase_coordinates("a1 a2"))
+    expected = "A1 A2"
+
+    assert_equal expected, actual
+  end
+
+  def test_get_letters
+    board = Board.new
+    actual = board.get_letters(["A1", "A2"])
+    expected = ["A", "A"]
+
+    assert_equal expected, actual
+  end
+
+  def test_get_numbers
+    board = Board.new
+    actual = board.get_numbers(["A1", "A2"])
+    expected =["1", "2"]
+
+    assert_equal expected, actual
+  end
+
+  def test_detect_direction
+    skip
+    board = Board.new
+    expected = "horizontal"
+    actual = board.detect_direction(["A1", "A2"])
+
+    assert_equal expected, actual
+  end
+
 end
